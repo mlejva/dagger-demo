59dce030635b40c4b9eae222e9bca1345b338949
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-03-18 15:58:48 -0700

    Add buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
 import './App.css';
 
-import { useDevbook } from '@devbookhq/sdk';
+import {
+  useDevbook,
+  DevbookStatus,
+} from '@devbookhq/sdk';
 import {
   Terminal,
   Filesystem,
+  TerminalHandler,
+  Button,
 } from '@devbookhq/ui';
 
 const devbookOptions = {
@@ -15,11 +21,38 @@ const devbookOptions = {
 
 function App() {
   const devbook = useDevbook(devbookOptions)
+  const terminalRef = useRef<TerminalHandler>(null)
+
+
+  function goTodoapp() {
+    if (devbook.status !== DevbookStatus.Connected) return
+    if (!terminalRef.current) return
+
+    terminalRef.current.handleInput('cd /dagger/pkg/universe.dagger.io/examples/todoapp')
+    terminalRef.current.focus()
+  }
+
+  function build() {
+    if (devbook.status !== DevbookStatus.Connected) return
+    if (!terminalRef.current) return
+
+    terminalRef.current.handleInput('dagger do build')
+    terminalRef.current.focus()
+  }
 
   return (
     <div className="App">
+      <Button
+        onClick={goTodoapp}
+        text="Go to todoapp"
+      />
+      <Button
+        onClick={build}
+        text="Build"
+      />
       <div className="terminal">
         <Terminal
+          ref={terminalRef}
           title="Dagger"
           devbook={devbook}
           height="500px"
